c74011374ef70ff6aad2a0a27263159e5325f16c
Julio Guzman <julio.guzman@globant.com>
2017-12-03 18:38:46 -0600

    Initial Commit

---
+++ Bitso/Bitso.h
@@ -0,0 +1,19 @@
+//
+//  Bitso.h
+//  Bitso
+//
+//  Created by Julio Cesar Guzman Villanueva on 12/3/17.
+//  Copyright © 2017 Julio Cesar Guzman Villanueva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bitso.
+FOUNDATION_EXPORT double BitsoVersionNumber;
+
+//! Project version string for Bitso.
+FOUNDATION_EXPORT const unsigned char BitsoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bitso/PublicHeader.h>
+
+
